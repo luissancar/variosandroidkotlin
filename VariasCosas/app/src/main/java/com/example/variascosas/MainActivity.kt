8a5f306a0220fab96e945897270c376b4a75cf24
luissancar <luissancar@yahoo.es>
2020-10-02 11:55:58 +0200

    landscape

--- VariasCosas/app/src/main/java/com/example/variascosas/MainActivity.kt
+++ VariasCosas/app/src/main/java/com/example/variascosas/MainActivity.kt
@@ -1,25 +1,35 @@
 package com.example.variascosas
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.Surface
 import android.widget.EditText
 import android.widget.Toast
-import androidx.core.widget.addTextChangedListener
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button.isEnabled=false
-        editTextTextPersonName.afterTextChanged {
-            Toast.makeText(this,editTextTextPersonName.getText().toString(),Toast.LENGTH_SHORT).show()
-            button.isEnabled=editTextTextPersonName.text.isNotEmpty()
+        val rotacion = windowManager.defaultDisplay.rotation
+        if (rotacion == Surface.ROTATION_0 || rotacion == Surface.ROTATION_180) {
+            button.isEnabled = false
+            editTextTextPersonName.afterTextChanged {
+
+                button.isEnabled = editTextTextPersonName.text.isNotEmpty()
+
+            }
+        }
+        else
+            {
+            //...hacer lo que quiera con la pantalla horizontal
+        }
 
         }
-    }
+
+
 
 
     fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
